d8a97472e51517a78dd0234e51c9ed8dcaf2b7b4
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-07-22 02:50:01 -0300

    Condição Simples

---
+++ ex005/aula08.js
@@ -0,0 +1,8 @@
+var vel = 40.5
+console.log(`A velocidade do carro é de ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+// Condição Simples em Javascript
