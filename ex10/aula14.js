85a491ca4917e336651c7b47f2547479a0724570
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-07-24 00:09:53 -0300

    diferença existente entre while, do..while e for

---
+++ ex10/aula14.js
@@ -0,0 +1,12 @@
+console.log('Vai começar...')
+for(var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+
+/* repetições com variáveis de controle em JavaScript (for)
+for (var c = 1;c <= 10;c++ ) {
+    console.log(c)
+}
+*/
