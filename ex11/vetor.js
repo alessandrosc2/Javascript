d9c53544464669d8ba73133e6fceaf814b73fb08
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-07-26 00:20:25 -0300

    Adicionando comentários

--- ex11/vetor.js
+++ ex11/vetor.js
@@ -1,5 +1,9 @@
 var valores = [2, 9, 5,7]
 
+//Ordenar com sort()
+valores.sort()
+
+//Mostrar valores
 console.log(valores)
 
 /* for (pos = 0;pos < valores.length;pos++) {
@@ -7,7 +11,16 @@ console.log(valores)
 }
 */
 
+/*
 //Código simplificado
 for(var pos in valores){
     console.log(valores[pos])
+    
 }
+*/
+//Código simplificado com comentário
+for(var pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    
+}
+
