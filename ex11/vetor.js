e76122067bf82fe5393455421b7bd9b6bae8b39e
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-07-26 00:16:57 -0300

    código simplificado

--- ex11/vetor.js
+++ ex11/vetor.js
@@ -2,6 +2,12 @@ var valores = [2, 9, 5,7]
 
 console.log(valores)
 
-for (pos = 0;pos < valores.length;pos++) {
+/* for (pos = 0;pos < valores.length;pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+
+//Código simplificado
+for(var pos in valores){
+    console.log(valores[pos])
 }
