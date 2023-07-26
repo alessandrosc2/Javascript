a96d7b2fd7d8223905566b6633de66718841b007
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-07-26 00:13:44 -0300

    Vetores

--- ex11/vetor.js
+++ ex11/vetor.js
@@ -1,12 +1,7 @@
-var num = [2, 9, 5,7]
-num.push(6)
-num[5] = 4
-num.sort()
-console.log(num)
-console.log(num[3])
-console.log(`Nosso número é ${num}`)
-console.log(`O vetor tem ${num.length} posições`)
-console.log(`O Primeiro número do vetor é ${num[0]}`)
+var valores = [2, 9, 5,7]
 
-for (pos = 0;pos < num.length;pos++)
-console.log(num[pos])
+console.log(valores)
+
+for (pos = 0;pos < valores.length;pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
