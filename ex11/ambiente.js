83fb26c3f004053be13c868374963177c84a00cc
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-07-26 00:08:09 -0300

    vteores

---
+++ ex11/ambiente.js
@@ -0,0 +1,11 @@
+var num = [2, 9, 5,7]
+num.push(6)
+num[5] = 4
+num.sort()
+console.log(num)
+console.log(num[3])
+console.log(`Nosso número é ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O Primeiro número do vetor é ${num[0]}`)
+
+
