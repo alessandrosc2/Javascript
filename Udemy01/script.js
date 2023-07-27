9c6ed538f317a7c65e7ae31ae5e545df19428d88
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-07-26 23:25:17 -0300

    aula sobre const em JS

---
+++ Udemy01/script.js
@@ -0,0 +1,11 @@
+const nome = 'Alessandro Dos Santos Cordeiro'
+const idade = 44
+const peso = 80
+const altura = 1.75
+const ano = 2023
+let imc = peso / (altura * altura)
+let anoNascimento = ano - idade
+
+console.log(`${nome} tem ${idade}, e pesa ${peso}, tem ${altura} de altura e seu IMC é de ${imc} `)
+console.log(`${nome} nasceu no ano de ${anoNascimento}`)
+
